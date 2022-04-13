bf7efa6b674f90ac4ee13b5495477746553be126
noam-z1 <noam.z1@gmail.com>
2022-04-13 14:38:27 +0300

    implementing file interceptors

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,20 +1,32 @@
-import { Controller, Post, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Body, Controller, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { diskStorage} from 'multer';
 
 @Controller('/files')
 export class FilesController {
   @Post()
   @UseInterceptors(
-    FileInterceptor('file'
-    // , {
-    //   storage: diskStorage({
-    //     destination: './files',
-    //   })
-    // }
-    )
+    FileFieldsInterceptor([
+      { name: 'patients', maxCount: 1 },
+      { name: 'treatments', maxCount: 1 },
+    ], {
+      storage: diskStorage({
+            destination: './files',
+            filename: (req, file, callback) => {
+              // ToDo: change to token
+              const hospitalId = req.headers.hospitalid;
+              const date = new Date().toISOString().slice(0, 10);
+              callback(null, `${hospitalId}.${file.fieldname}.${date}.csv`);
+            }
+          })
+    })
+
   )
-  async uploadFile() {
-    return 'All good!';
+  async uploadFile(
+    @Body() body: any,
+    @UploadedFiles() files: {patients: Express.Multer.File[], treatments: Express.Multer.File[]},
+    ) {
+      console.log("file", files)
+      console.log("body", body)
   }
 }
